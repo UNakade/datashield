645ab56ac157020e9c83542bf32bc250ed95e947
Uday Nakade <83889634+UNakade@users.noreply.github.com>
2021-07-31 10:24:23 +0200

    Add example.py, update README

--- README.md
+++ README.md
@@ -9,50 +9,22 @@ The plugin is written in `Python` and uses the `rpy2` library.
   - `rpy2` (`pip install rpy2[all]`)
   - `numpy`
   - `json`
-- In addition to `R`, these `R` libraries:
-  - `DSI`
-  - `DSOpal`
-  - `DSLite`
-  - `dsBaseClient` (for installation instructions, please visit the [DataSHIELD website](https://www.datashield.org/) and the [DataSHIELD wiki](https://data2knowledge.atlassian.net/wiki/spaces/DSDEV/overview))
-  - `fields`
-  - `metafor`
-  - `ggplot2`
-  - `gridExtra`
-  - `data.table`
-- `curl` library for your operating system
+- `libcurl` and `openssl` libraries for your operating system
+- In addition to `R (>= v3.5.0)`, these `R` packages:
+  - `curl` and `openssl`
+  - `DSI`, `DSOpal`
+  - `fields`, `metafor`, `ggplot2`, `gridExtra` and `data.table` (dependencies of `dsBaseClient`)
+  - `dsBaseClient` (from the `http://cran.obiba.org` repository. For installation instructions, please visit the [DataSHIELD website](https://www.datashield.org/) and the [DataSHIELD wiki](https://data2knowledge.atlassian.net/wiki/spaces/DSDEV/overview))
 
-### Usage:
-Here, it is assumed that a DataSHIELD virtual machine is currently accessible at the IP address `192.168.56.100:8080` (please visit the [DataSHIELD wiki](https://data2knowledge.atlassian.net/wiki/spaces/DSDEV/overview) for instructions about how to get it running) and the `fdrtd` server is running on `localhost` on port `5000`.
 
-```Python
-# First, initialize the fdrtd api:
-import fdrtd
-interface = fdrtd.HttpInterface("http://localhost:5000")  # insert appropriate server url here
-api = fdrtd.Api(interface)
-# Next, select the datashield microservice:
-connection = api.select_microservice(microservice='datashield')
-# Define required variables for logging in:
-# list of servers is a list of kwargs dictionaries which will be passed on to the 
-# DSI::newDSLoginBuilder()$append() function
-list_of_servers = [
-    {"server": "study1", "url": "http://192.168.56.100:8080/", "user": "administrator", "password": "datashield_test&",
-     "table": "CNSIM.CNSIM1", "driver": "OpalDriver"},
-    {"server": "study2", "url": "http://192.168.56.100:8080/", "user": "administrator", "password": "datashield_test&",
-     "table": "CNSIM.CNSIM2", "driver": "OpalDriver"},
-    {"server": "study3", "url": "http://192.168.56.100:8080/", "user": "administrator", "password": "datashield_test&",
-     "table": "CNSIM.CNSIM3", "driver": "OpalDriver"}
-]
-assign = True
-symbol = 'D'
-login_return = connection.login(parameters={'list_of_servers': list_of_servers, 'assign': assign, 'symbol': symbol})
-# You can now start using dsBaseClient functions in one of two ways:
-quantileMean_1 = connection.quantileMean(parameters={'x': 'D$LAB_HDL'})
-quantileMean_2 = connection.call_function(parameters={'x': 'D$LAB_HDL'}, func='ds.quantileMean')
-# If there is a '.' in the function name, 
-# please replace them by '_' if calling the function without specifying the 'func' argument
-# e.g. if you want to call ds.matrixDet.report, either use connection.matrixDet_report(parameters)
-# or use connection.call_function(parameters, func='ds.matrixDet.report')
-
-# To logout from the DataSHIELD servers:
-logout_return = connection.logout()
+### Installation:
+Installing and running `fdrtd_server` with the `protocol_DataSHIELD` is very simple and takes only 4 commands:
+```shell
+git clone https://github.com/fdrtd/server
+cd server
+git clone https://github.com/fdrtd/server/protocol_DataSHIELD
+python3 -m openapi_server -port:5000 
 ```
+And that's all! You now have a `fdrtd_server` running on `http://localhost:5000`!
+
+For a detailed example on how to use the `protocol_DataSHIELD` on the client side, please refer `protocol_DataSHIELD/src/example.py`.
---
+++ examples/example.py
@@ -0,0 +1,77 @@
+# This example assumes that the fdrtd_server is running on http://localhost:5000
+# which is very easy to do, just follow these 4 steps:
+# git clone https://github.com/fdrtd/server
+# cd server
+# git clone https://github.com/fdrtd/server/protocol_DataSHIELD
+# python3 -m openapi_server -port:5000
+# and that's all! The fdrtd server is now running on http://localhost:5000!
+
+# Now on the client side:
+# Initializing the fdrtd api
+import fdrtd
+interface = fdrtd.HttpInterface("http://localhost:5000")  # insert appropriate server url here
+api = fdrtd.Api(interface)
+
+# Selecting the DataSHIELD login microservice
+login = api.select_microservice(protocol='DataSHIELD', microservice='login')
+# Defining required variables for logging in:
+# list_of_servers is a list of kwargs dictionaries which will be passed on to the
+# DSI::newDSLoginBuilder()$append() function
+# This example assumes that a datashield virtual machine is available at http://192.168.56.100:8080/. For instructions
+# on how to get it running, please visit the DataSHIELD wiki (link in README).
+
+list_of_servers = [
+    {"server": "study1", "url": "http://192.168.56.100:8080/", "user": "administrator", "password": "datashield_test&",
+     "table": "CNSIM.CNSIM1", "driver": "OpalDriver"},
+    {"server": "study2", "url": "http://192.168.56.100:8080/", "user": "administrator", "password": "datashield_test&",
+     "table": "CNSIM.CNSIM2", "driver": "OpalDriver"},
+    {"server": "study3", "url": "http://192.168.56.100:8080/", "user": "administrator", "password": "datashield_test&",
+     "table": "CNSIM.CNSIM3", "driver": "OpalDriver"}
+]
+
+login_callback = login.login(list_of_servers=list_of_servers, assign=True, symbol='D')
+# with protocol_DataSHIELD, you can print the progress of any function live, just like it is visible in R
+# when a function is called, a function_callback is returned to the client while the function keeps running on the
+# server in a separate thread.
+# while it is running, you can use the following "result" function to get the live progress bar
+# it will also return the end result of the function call
+
+def result(function_callback):
+    status_old = function_callback.get_status()
+    print(''.join(status_old['warnerror']), end='')
+    while status_old['busy']:
+        status_new = function_callback.get_status()
+        if len(status_new['warnerror']) != len(status_old['warnerror']):
+            print(''.join(status_new['warnerror'][len(status_old['warnerror']):]), end='')
+        status_old = status_new
+    print(''.join(status_old['print']))
+    return function_callback.get_result()
+
+connection_callback = result(login_callback)
+# The end result of a login function called on the login microservice is a connection callback
+# You can use it to start calling functions from the dsBaseClient library
+# There are multiple convenient ways of calling these functions.
+# You can either pass the arguments to the function directly as kwargs, or you can specify them as a dictionary
+# and pass it as the kwarg "parameters". e. g.:
+# connection_callback.quantileMean(x='D$LAB_HDL')
+# connection_callback.quantileMean(parameters={'x':'D$LAB_HDL'})
+# please note that "ds." has been removed from the beginning of the function name.
+# Also, the "datasources" kwarg is not
+# to be used here. By default, all the servers in the connection are passed on to the "datasources" argument. If
+# you need to perform the computation on only a limited number of servers, please include the argument "servers" as
+# a list of indices as R would index them (indexing begins at 1 in R and at 0 in Python).
+# Another way of calling the functions is:
+# connection_callback.call_function(func='ds.quantileMean', x='D$LAB_HDL')
+# connection_callback.call_function(func='ds.quantileMean', parameters={'x':'D$LAB_HDL'})
+
+# The client should note that in Python, the "." character is reserved whereas in R, it can be freely used in
+# variable names. Hence, if passing an argument that contains a "." in the name, please replace it by an "_" or
+# use the parameters dictionary instead. Same goes for function names. e. g.:
+# connection_callback.matrixDet_report(parameters) (instead of matrixDet.report)
+# connection_callback.dataFrame(parameters, row_names) (instead of row.names)
+
+# Full example:
+quantileMean_result = result(connection_callback.quantileMean(x='D$LAB_HDL'))
+
+# To logout from the DataSHIELD servers:
+logout_result = result(connection_callback.logout())
